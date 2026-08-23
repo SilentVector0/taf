9e0fe74ab5a919b568d8c049d4ee66f2f6da6259
mathieu <mathieusuter@ahoo.com>
2026-08-23 18:28:17 +0200

    avancement de mon ex01

--- CPP/cpp_module00/ex01/PhoneBook.cpp
+++ CPP/cpp_module00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: msuter <msuter@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/23 15:57:35 by msuter            #+#    #+#             */
-/*   Updated: 2026/08/23 16:40:38 by msuter           ###   ########.fr       */
+/*   Updated: 2026/08/23 18:26:59 by msuter           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,31 @@ PhoneBook::PhoneBook()
 	pos = 0;
 }
 
+static std::string loop()
+{
+	std::string value;
+
+	
+}
+
+void PhoneBook::Add()
+{
+	std::string Value;
+
+	std::cout << "Name:\n";
+	std::getline(std::cin, Value);
+	C[pos].SetName(Value);
+	std::cout << " Last Name:\n";
+	std::getline(std::cin, Value);
+	C[pos].SetLastName(Value);
+	std::cout << "Nickname:\n";
+	std::getline(std::cin, Value);
+	C[pos].SetNickName(Value);
+	std::cout << "Phone Number:\n";
+	std::getline(std::cin, Value);
+	C[pos].SetPhoneNumber(Value);
+	std::cout << "darkest secret:\n";
+	std::getline(std::cin, Value);
+	C[pos].SetDarkestSecret(Value);
+	return;
+}
